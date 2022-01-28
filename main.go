deda4facd853324de60d460b4575837d94eb8736
Memo Chou <memochou1993@gmail.com>
2022-01-28 15:57:25 +0800

    Add template

--- main.go
+++ main.go
@@ -1,25 +1,81 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	outputDir = "dist"
+)
+
 type Thesaurus struct {
-	Subjects []Subject `json:"subjects"`
+	Subjects []Subject `yaml:"subjects"`
 }
 
 type Subject struct {
 	ParentRelationship struct {
-		PreferredParents    []Term `json:"preferredParents"`
-		NonPreferredParents []Term `json:"nonPreferredParents"`
-	} `json:"parentRelationship"`
+		PreferredParents    []Term `yaml:"preferredParents"`
+		NonPreferredParents []Term `yaml:"nonPreferredParents"`
+	} `yaml:"parentRelationship"`
 	Term struct {
-		PreferredTerms    []Term `json:"preferredTerms"`
-		NonPreferredTerms []Term `json:"nonPreferredTerms"`
-	} `json:"term"`
-	DescriptiveNotes []Note `json:"descriptiveNotes"`
+		PreferredTerms    []Term `yaml:"preferredTerms"`
+		NonPreferredTerms []Term `yaml:"nonPreferredTerms"`
+	} `yaml:"term"`
+	DescriptiveNotes []Note `yaml:"descriptiveNotes"`
 }
 
 type Term struct {
-	TermText string `json:"termText"`
+	TermText string `yaml:"termText"`
 }
 
 type Note struct {
-	NoteText string `json:"noteText"`
+	NoteText string `yaml:"noteText"`
+}
+
+var (
+	thesaurus Thesaurus
+)
+
+func main() {
+	if err := parse(); err != nil {
+		log.Fatal(err)
+	}
+	if err := print(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func parse() error {
+	b, err := ioutil.ReadFile("thesaurus.yaml")
+	if err != nil {
+		return err
+	}
+	if err = yaml.Unmarshal(b, &thesaurus); err != nil {
+		return err
+	}
+	for _, term := range thesaurus.Subjects {
+		fmt.Println(term.Term.PreferredTerms)
+	}
+	return nil
+}
+
+func print() error {
+	b, err := ioutil.ReadFile("template.html")
+	if err != nil {
+		return err
+	}
+	s := strings.Replace(string(b), "__BODY__", "hello", 1)
+	if _, err = os.Stat(outputDir); os.IsNotExist(err) {
+		if err = os.MkdirAll(outputDir, 0755); err != nil {
+			return err
+		}
+	}
+	filename := fmt.Sprintf("%s/index.html", outputDir)
+	return ioutil.WriteFile(filename, []byte(s), 0755)
 }
