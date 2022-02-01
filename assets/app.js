48ea58151a3a53e6ac3aca274f636bc44fec79ce
Memo Chou <memochou1993@gmail.com>
2022-02-02 03:26:11 +0800

    Add template for subject list

--- assets/app.js
+++ assets/app.js
@@ -1,36 +1,41 @@
+/**
+ * @param {HTMLElement} target
+ * @param {Object} prop
+ * @param {Object} prop.subject
+ * @param {Object} prop.subject.term
+ * @param {Array} prop.subject.term.preferredTerms
+ * @param {string} prop.subject.term.preferredTerms[].termText
+ * @param {Object} prop.subject.note
+ * @param {Array} prop.subject.note.descriptiveNotes
+ * @param {string} prop.subject.note.descriptiveNotes[].noteText
+ * @param {Array} prop.children
+ */
+const render = (target, prop) => {
+  const template = document.querySelector('[data-template]');
+  const [subject] = template.content.cloneNode(true).children;
+  const [term, note, children] = subject.children;
+  prop.subject.term.preferredTerms.forEach((v) => {
+    term.textContent = v.termText;
+    term.classList.add(prop?.children?.length ? 'term-expandable' : 'term-expanded');
+  });
+  prop.subject.note?.descriptiveNotes?.forEach((v) => {
+    const child = document.createElement('div');
+    child.textContent = v.noteText;
+    note.append(child);
+  });
+  prop.children?.forEach((c) => render(children, c));
+  target.appendChild(subject);
+};
 (async () => {
-  const list = document.querySelector('#list');
   const data = await fetch('data.json').then((r) => r.json());
-  const render = (target, root) => {
-    const subject = document.createElement('li');
-    subject.setAttribute('class', 'subject');
-    target.appendChild(subject);
-    root.subject.term.preferredTerms.forEach((v) => {
-      const term = document.createElement('div');
-      term.innerHTML = v.termText;
-      term.setAttribute('class', root?.children?.length ? 'term term-expandable' : 'term term-expanded');
-      subject.appendChild(term);
-    });
-    root.subject.note?.descriptiveNotes?.forEach((v) => {
-      const note = document.createElement('div');
-      note.innerHTML = v.noteText;
-      note.setAttribute('class', 'note');
-      subject.appendChild(note);
-    });
-    const children = document.createElement('ul');
-    children.setAttribute('class', 'children');
-    subject.appendChild(children);
-    root.children?.forEach((c) => render(children, c));
-  };
-  render(list, data.root);
-
+  const title = document.querySelector('#title');
+  title.textContent = data.title;
+  const list = document.querySelector('#list');
   list.addEventListener('click', (e) => {
     if (e.target.classList.contains('term-expandable')) {
       e.target.parentElement.querySelector('.children').classList.toggle('active');
       e.target.classList.toggle('term-expanded');
     }
   });
-
-  const title = document.querySelector('#title');
-  title.innerHTML = data.title;
+  render(list, data.root);
 })();
